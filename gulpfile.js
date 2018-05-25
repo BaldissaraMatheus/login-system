1712479e2bb89691985edbfd605ea91dd3d500b3
BaldissaraMatheus <baldissaramatheus@gmail.com>
2018-05-25 11:16:21 -0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const minify = require('gulp-image');
+
+gulp.task('js', () =>{
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'src/js/*.js'])
+    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('sass', () => {
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('php', () =>{
+  return gulp.src(['src/index.php'])
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('image', () =>{
+  return gulp.src(['src/assets/*.png', 'src/assets/*.jpg', 'src/assets/*.svg'])
+    .pipe(minify())
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('serve', ['sass'], () => {
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+  gulp.watch(['src/js/*.js'], ['js']);
+  gulp.watch(['src/js/*'], ['image']);
+  gulp.watch(['src/*.php'], ['php']);
+});
+
+gulp.task('default', ['sass', 'js', 'php', 'image', 'serve']);
---
+++ src/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <title>Wubba lubba dub dub</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+  <link rel='stylesheet' type='text/css' href='/css/bootstrap.css'>
+  <link rel='stylesheet' type='text/css' href='/css/main.css'>
+</head>
+
+<body>
+  <div class='container'>
+    <header>
+      <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <a href='/' class='navbar-brand'>Cachorro Fumaca</a>
+        
+        <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent'>
+          <span class='navbar-toggler-icon'></span>
+        </button>
+
+        <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul class='navbar-nav ml-auto'>
+
+            <li class='nav-item'>
+              <form class='form-inline' action=''>
+                
+                <label for='inlineFormInputGroupUsername' class='sr-only'>Username</label>
+                <div class='input-group'>
+                  <div class='input-group-prepend'>
+                    <div class='input-group-text'>@</div>
+                  </div>
+                  <input type='text' class='form-control mr-2' placeholder='Nome de usuário ou email'>
+                </div>
+
+                <label for='inputPassword' class='sr-only'>Senha</label>
+                <input type='password' class='form-control' placeholder='Senha'>
+
+                <button type='submit' class='btn btn-primary ml-2'>Entrar</button>
+
+              </form>
+            </li>
+
+            <li class='nav-item'>
+              <a href='#' class='nav-link'>Ainda não sou cadastrado</a>
+            </li>        
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+    <main class='jumbotron'>
+      <h1 class='display-4'>Simples. Elegante. Supimpa.</h1>
+      <p class='lead'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quia porro unde, labore, iste obcaecati nisi expedita sit harum distinctio perspiciatis nihil incidunt nesciunt voluptatibus, ex reprehenderit itaque praesentium? Et.</p>
+      <hr class='my-4'>
+      <p class='lead'>
+        <a href='#' class='btn btn-primary btn-lg' role='button'>Ler mais</a>
+      </p>
+    </main>
+
+  </div>
+  <script src='/js/jquery.min.js'></script>
+  <script src='/js/popper.min.js'></script>
+  <script src='/js/bootstrap.min.js'></script>
+</body>
+</html>
